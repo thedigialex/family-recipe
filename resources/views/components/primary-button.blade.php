8ddcbf27f8ae1f87549265afd8a68ab8858d4602
Alejandro Martinez <alejandro11raym@gmail.com>
2024-06-01 17:47:14 -0400

    Minor styling changes

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-3 py-2 bg-accent hover:bg-darkaccent text-backgroundLightAccent hover:text-backgroundAccent font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition ease-in-out duration-150 transform hover:scale-105']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-3 py-2 bg-accent hover:bg-darkaccent text-backgroundAccent hover:text-backgroundLightAccent font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition ease-in-out duration-150 transform hover:scale-105']) }}>
     {{ $slot }}
 </button>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,81 +1,78 @@
-<nav x-data="{ open: false }" class="bg-primary">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
+<nav x-data="{ open: false }">
+    <div class="bg-primary">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="flex justify-between h-16 items-center">
+
+                <div class="flex items-center">
                     <a href="{{ route('recipes.index') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current" />
+                        <x-application-logo class="block h-16 w-auto fill-current rounded-3xl" />
                     </a>
                 </div>
-            </div>
-        </div>
-        <div class="flex justify-between h-16">
-            <div class="flex">
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex items-center">
-                    <x-nav-link :href="route('recipes.index')" :active="request()->routeIs('recipes.index')">
-                        {{ __('Recipes') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('families.create')" :active="request()->routeIs('families.create')">
-                        {{ __('Create Family') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('families.joinForm')" :active="request()->routeIs('families.joinForm')">
-                        {{ __('Join Family') }}
-                    </x-nav-link>
-                    @if(Auth::user()->headedFamilies->isNotEmpty())
+                <div class="flex items-center">
+                    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                        <div class="hidden sm:flex justify-center h-16 items-center space-x-8">
+                            <x-nav-link :href="route('recipes.index')" :active="request()->routeIs('recipes.index')">
+                                {{ __('Recipes') }}
+                            </x-nav-link>
+                            <x-nav-link :href="route('families.create')" :active="request()->routeIs('families.create')">
+                                {{ __('Create Family') }}
+                            </x-nav-link>
+                            <x-nav-link :href="route('families.joinForm')" :active="request()->routeIs('families.joinForm')">
+                                {{ __('Join Family') }}
+                            </x-nav-link>
+                            @if(Auth::user()->headedFamilies->isNotEmpty())
+                            <x-dropdown align="right" width="48">
+                                <x-slot name="trigger">
+                                    <button class="flex items-center">
+                                        <x-nav-link>{{ __('Manage Family') }}</x-nav-link>
+                                    </button>
+                                </x-slot>
+
+                                <x-slot name="content">
+                                    @foreach(Auth::user()->headedFamilies as $family)
+                                    <x-dropdown-link :href="route('families.manage', $family->id)">
+                                        {{ $family->name }}
+                                    </x-dropdown-link>
+                                    @endforeach
+                                </x-slot>
+                            </x-dropdown>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+                <div class="hidden sm:flex items-center space-x-4">
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
-                            <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                                <div>{{ __('Manage Family') }}</div>
-                            </button>
+                            <x-primary-button>
+                                <div>{{ Auth::user()->name }}</div>
+                            </x-primary-button>
                         </x-slot>
 
                         <x-slot name="content">
-                            @foreach(Auth::user()->headedFamilies as $family)
-                            <x-dropdown-link :href="route('families.manage', $family->id)">
-                                {{ $family->name }}
+                            <x-dropdown-link :href="route('profile.edit')">
+                                {{ __('Profile') }}
                             </x-dropdown-link>
-                            @endforeach
+
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+
+                                <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                                    {{ __('Log Out') }}
+                                </x-dropdown-link>
+                            </form>
                         </x-slot>
                     </x-dropdown>
-                    @endif
                 </div>
-            </div>
-            <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <x-primary-button>
-                            <div>{{ Auth::user()->name }}</div>
-                        </x-primary-button>
-                    </x-slot>
 
-                    <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
-                        </x-dropdown-link>
-
-                        <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-
-                            <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
-                    </x-slot>
-                </x-dropdown>
-            </div>
-
-            <!-- Hamburger -->
-            <div class="-me-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
+                <div class="-me-2 flex items-center sm:hidden">
+                    <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                        <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                            <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                            <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
             </div>
         </div>
     </div>
@@ -83,8 +80,14 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('recipes.index')" :active="request()->routeIs('recipes.index')">
+                {{ __('Recipes') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('families.create')" :active="request()->routeIs('families.create')">
+                {{ __('Create Family') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('families.joinForm')" :active="request()->routeIs('families.joinForm')">
+                {{ __('Join Family') }}
             </x-responsive-nav-link>
         </div>
 
